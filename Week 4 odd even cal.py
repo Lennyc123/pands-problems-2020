9eb67bceaf1e725851a742a1ce58b48c8b43b296
Lennyc123 <G00330875@gmit.ie>
2020-02-05 13:30:52 +0000

    Comments added to week 4 (odd even cal) in order to provide explanation of the code.

--- Week 4 odd even cal.py
+++ Week 4 odd even cal.py
@@ -1,21 +1,20 @@
-#Input any positive integer 
-# - Then output succcessive values of a calculation
+#The following program promts a user to input any positive integer
+# - then outputs the successive values of the following calculation
 
-#At each step calculate the next value by taking the current value and -
-#If  - even divide by two,
-#else -  odd multiply by 3 and add 1
+# If - even, divide by 2
+# otherwise (else) odd - multiply by 3 and add 1.
 
-#End program if current value is 1
+# The program is then set to end if the current value is one.
 
-x = int(input("Enter any positive integer:"))
-while x != 1:
-    if x % 2 == 0:
-        print(round(x))
-        x = x / 2
-    #elif x % 2 != 0:
+x = int(input("Enter any positive integer:")) # User is prompted to input any positive integer
+while x != 1: # While loop will end upon reaching 1.
+    if x % 2 == 0: # Provides a sequence on what to do if the calculation is even. i.e. is divisable by 2 or leaves no remainder
+        print(round(x)) # value of x is displayed for the user and is rounded.
+        x = x / 2 # the value of x is divided by 2
+    #elif x % 2 != 0: # explanation of what the else command is calculating. i.e. if the number cannot be divided by 2 equally, it is odd.
     else:
-        print(round(x))
-        x = x * 3 + 1
-print("1")
+        print(round(x)) # value of x is displayed for the user and is rounded.
+        x = x * 3 + 1 # value of x is multiplyed by 3 and 1 is added.
+print("1") # value of 1 is printed.
         
 
