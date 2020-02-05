117097510f98a8ce0ae2755df10383ad111645bf
Lennyc123 <G00330875@gmit.ie>
2020-02-05 13:10:14 +0000

    (Week 3, task) reverse, second character added.

---
+++ secondstring.py
@@ -0,0 +1,6 @@
+# The following program (python code), 
+# retrieves an input and outputs each second character in a reverse order.
+
+s = str(input("please enter a sentence:")) #Promts the user to input a sentence
+print(s[::-2]) # The sentence inputted by the user is then ouputted in a reverse order,
+                # displaying only each second charcter
