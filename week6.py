6e846e1b13850ba85fcf069b8747dcee77b2fe87
Lennyc123 <G00330875@gmit.ie>
2020-02-21 11:26:04 +0000

    addition of code for SQRT, w. comments.

--- week6.py
+++ week6.py
@@ -10,12 +10,29 @@
 # Add the approximate root with the original number divided by the approximate root and divide by 2.
 # x_i := (x_i + n / x_i) / 2
 
-x = float(input("Enter any positive decimal number"))
+# xr = xrough i.e approximation
 
-def sqrt(x):
-    xr = x 
-    return (xr + x / xr) / 2
+# Difficulty in understanding how to add while loop
+# Difficulty - how to code the following instruction
+# Continue until the difference in the approximate root along the iterations is less than the desired value (or precision value).
+
+#def sqrt(x):
+ #   xr = x 
+ #   return (xr + x / xr) / 2
     #while xr < x:
         #(xr + x / xr) / 2
-    
+
+# x = float(input("Enter any positive decimal number"))
+# print("The square root of" , x , "is approx." , sqrt(x))
+
+def sqrt(x):
+    r = x 
+    precision = 10 ** (-10)
+
+    while abs(x -r * r) > precision:
+        r = (r + x / r) / 2
+
+    return r
+
+x = float(input("Enter any positive decimal number"))
 print("The square root of" , x , "is approx." , sqrt(x))
