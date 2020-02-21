0c97b7ef9ef94868a78c5d3c58c4a5db128dcdbb
Lennyc123 <G00330875@gmit.ie>
2020-02-21 12:42:03 +0000

    algorithm within function and references added.

--- week6.py
+++ week6.py
@@ -1,38 +1,19 @@
-# $ python squareroot.py
-# Please enter a positive number: 14.5
-# The square root of 14.5 is approx. 3.8.
+# The following program takes a positive floating-point number as an input then,
+# Outputs an approximation of its square root.
 
-# Program that takes positive floating-point number as input
-# Outputs approximation of its square root.
-# Create function called sqrt
-
-#Algorithm approximate root = x_i
-# Add the approximate root with the original number divided by the approximate root and divide by 2.
-# x_i := (x_i + n / x_i) / 2
-
-# xr = xrough i.e approximation
-
-# Difficulty in understanding how to add while loop
-# Difficulty - how to code the following instruction
-# Continue until the difference in the approximate root along the iterations is less than the desired value (or precision value).
-
-#def sqrt(x):
- #   xr = x 
- #   return (xr + x / xr) / 2
-    #while xr < x:
-        #(xr + x / xr) / 2
-
-# x = float(input("Enter any positive decimal number"))
-# print("The square root of" , x , "is approx." , sqrt(x))
-
-def sqrt(x):
-    r = x 
+def sqrt(x): # Creation of a function named "sqrt" given the value x
+    r = x # r = approximate root for the orginal value x
     precision = 10 ** (-10)
 
-    while abs(x -r * r) > precision:
-        r = (r + x / r) / 2
+    while abs(x -r * r) > precision: # Continue until the difference in r is less than precision value
+        r = (r + x / r) / 2 #Addition of the approximate root with the orginal input divided by r - result is then divided by 2. 
 
-    return r
+    return r # End execution of function and provides result
 
-x = float(input("Enter any positive decimal number"))
-print("The square root of" , x , "is approx." , sqrt(x))
+x = float(input("Enter any positive decimal number")) # User is prompted to provide an input
+print("The square root of" , x , "is approx." , sqrt(x)) # Approximation of initial input is provided
+
+# References
+# "Defining and Using Functions" section of A Whirlwind Tour of Python by Jake Vanderplas.
+# https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
+# https://www.geeksforgeeks.org/python-return-statement/
